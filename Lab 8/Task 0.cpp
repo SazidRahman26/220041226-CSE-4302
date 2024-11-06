4603e5e7faa3aaa3ea9dab4c126bfb83872d3fc4
SazidRahman <sazidurrahman@iut-dhaka.edu>
2024-11-06 10:06:47 +0600

    second commit

--- Lab 8/Task 0.cpp
+++ Lab 8/Task 0.cpp
@@ -9,8 +9,6 @@ private:
     string manufacturer;
     int size_limit;
 public:
-    Vehicle() : license_plate(""), manufacturer(""), size_limit(0)
-    {}
     Vehicle(string plate, string manufact, int lim) : license_plate(plate), manufacturer(manufact), size_limit(lim)
     {}
     void setLicensePlate(string s)
@@ -25,6 +23,18 @@ public:
     {
         size_limit = n;
     }
+    string setLicensePlate()
+    {
+        return license_plate;
+    }
+    string setManufacturer()
+    {
+        return manufacturer;
+    }
+    int setCarriageSizeLimit()
+    {
+        return size_limit;
+    }
 
 };
 
@@ -33,6 +43,8 @@ class GasolineVehicle : public Vehicle
 private:
     int fuel_capacity;
     string fuel_type;
+public:
+    GasolineVehicle(string license, string manufact, int lim, int fuel_cap, string ftype): Vehicle(license, manufact, lim), fuel_capacity(fuel_cap), fuel_type(ftype){}
 };
 
 class ElectricVehicle : public Vehicle
@@ -40,11 +52,14 @@ class ElectricVehicle : public Vehicle
 private:
     int battery_capacity;
     float charging_time;
+public:
+    ElectricVehicle(string license, string manufact, int lim, int bat_cap, float ctime): Vehicle(license, manufact, lim), battery_capacity(bat_cap), charging_time(ctime){}
 };
 
 class Motorcycle : public GasolineVehicle
 {
-private:
+public:
+    Motorcycle(string license, string manufact, int lim, int fuel_cap, string ftype) : GasolineVehicle(license, manufact, lim, fuel_cap, ftype){}
 
 };
 
@@ -52,18 +67,24 @@ class Car : public GasolineVehicle
 {
 private:
     int passenger_limit;
+public:
+    Car(string license, string manufact, int lim, int fuel_cap, string ftype, int plim) : GasolineVehicle(license, manufact, lim, fuel_cap, ftype), passenger_limit(plim){}
 };
 
 class Truck : public GasolineVehicle
 {
 private:
     int cargo_capacity;
+public:
+    Truck(string license, string manufact, int lim, int fuel_cap, string ftype, int ccap) : GasolineVehicle(license, manufact, lim, fuel_cap, ftype) , cargo_capacity(ccap) {}
 };
 
 class Hybrid : public GasolineVehicle , public ElectricVehicle
 {
 private:
     float energy_regen;
+public:
+    Hybrid(string license, string manufact, int lim, int fuel_cap, string ftype, int bat_cap, float ctime, float eregen) : GasolineVehicle(license, manufact, lim, fuel_cap, ftype), ElectricVehicle(license, manufact, lim, bat_cap, ctime), energy_regen(eregen){}
 };
 
 class Employee
@@ -71,11 +92,14 @@ class Employee
 private:
     string name;
     int id;
+public:
+    Employee(string Name, int Id) : name(Name), id(Id){}
 };
 
 class Manager: public Employee
 {
 public:
+    Manager(string Name, int Id) : Employee(Name, Id){}
     string getName()
     {
         return ("Mr. " + name);
@@ -85,6 +109,7 @@ public:
 class Driver: public Employee
 {
 public:
+    Driver(string Name, int Id) : Employee(Name, Id){}
     string getName()
     {
         return name;
--- Lab 8/Task 1.cpp
+++ Lab 8/Task 1.cpp
@@ -7,7 +7,10 @@ class Account
     string accountName;
     float balance;
   public:
-    Account(){}
+    Account(const string& name, float initialBalance, const string& accountPrefix): accountName(name), balance(bal)
+    {
+         accountNo = accountPrefix + "-" + to_string(nextAccountNo++);
+    }
     void setAccountNo(string s)
     {
         accountNo = s;
@@ -21,19 +24,181 @@ class Account
         balance = n;
     }
 
-    void getAccountNo()
+    string getAccountNo()
     {
         return accountNo;
     }
-    void getAccountName()
+    string getAccountName()
     {
         return Name;
     }
-    void getBalance()
+    float getBalance()
     {
         return balance;
     }
 };
 
 class CurrentAccount : public Accounts
-{}
+{
+    const static float serviceChage;
+    const static string accountPrefix;
+    static string nextAccount;
+public:
+    CurrentAccount(const string& name, float initialBalance)
+        : Account(name, initialBalance, accountPrefix) {}
+};
+
+class SavingsAccount : public Accounts
+{
+    float interestRate;
+    float monthlyDeopsitAmount;
+    const static string accountPrefix;
+    static string nextAccount;
+public:
+    SavingsAccount(const string& name, float initialBalance, float rate, float depositAmount)
+        : Account(name, initialBalance, accountPrefix), interestRate(rate), monthlyDepositAmount(depositAmount) {}
+    void setInterestRate(float n)
+    {
+        interestRate = n;
+    }
+    void setMonthlyDepositAmount(float n)
+    {
+        monthlyDeopsitAmount = n;
+    }
+    float getInterestRate(float n)
+    {
+        return interestRate;
+    }
+    float getMonthlyDepositAmount(float n)
+    {
+        return monthlyDeopsitAmount;
+    }
+    string nextAccountNo()
+    {
+        string temp = nextAccount;
+        temp[temp.size() - 1] += 1;
+        return temp;
+    }
+};
+
+class MonthlyDepositScheme : public Account
+{
+    float interestRate;
+    float monthlyDepositAmount;
+    const static string accountPrefix;
+    static string nextAccount;
+public:
+    MonthlyDepositScheme(const string& name, float initialBalance, float rate, float depositAmount, const string& accountPrefix)
+        : Account(name, initialBalance, accountPrefix), interestRate(rate), monthlyDepositAmount(depositAmount) {}
+    void setInterestRate(float n)
+    {
+        interestRate = n;
+    }
+    void setMonthlyDepositAmount(float n)
+    {
+        monthlyDepositAmount = n;
+    }
+    string nextAccountNo()
+    {
+        string temp = nextAccount;
+        temp[temp.size() - 1] += 1;
+        return temp;
+    }
+};
+
+class LoanAccount : public Account
+{
+    float interestRate;
+    float monthlyDepositAmount;
+    const static string accountPrefix;
+    static string nextAccount;
+public:
+    LoanAccount(const string& name, float initialBalance, float rate, float depositAmount)
+        : Account(name, initialBalance, accountPrefix), interestRate(rate), monthlyDepositAmount(depositAmount) {}
+
+    void setInterestRate(float n)
+    {
+        interestRate = n;
+    }
+    void setMonthlyDepositAmount(float n)
+    {
+        monthlyDepositAmount = n;
+    }
+    float getInterestRate(float n)
+    {
+        return interestRate;
+    }
+    float getMonthlyDepositAmount(float n)
+    {
+        return monthlyDeopsitAmount;
+    }
+    string nextAccountNo()
+    {
+        string temp = nextAccount;
+        temp[temp.size() - 1] += 1;
+        return temp;
+    }
+};
+
+class TwoYearMDS : public MonthlyDepositScheme
+{
+    float maximumInterest;
+public:
+    MonthlyDepositScheme(const string& name, float initialBalance, float rate, float depositAmount, const string& accountPrefix)
+        : Account(name, initialBalance, accountPrefix), interestRate(rate), monthlyDepositAmount(depositAmount) {}
+    void setMaximumInterest(float n)
+    {
+        maximumInterest = n;
+    }
+    float getMaximumInterest()
+    {
+        return maximumInterest;
+    }
+};
+
+class FiveYearMDS : public MonthlyDepositScheme
+{
+    float maximumInterest;
+public:
+      MonthlyDepositScheme(const string& name, float initialBalance, float rate, float depositAmount, const string& accountPrefix)
+        : Account(name, initialBalance, accountPrefix), interestRate(rate), monthlyDepositAmount(depositAmount) {}
+    void setMaximumInterest(float n)
+    {
+        maximumInterest = n;
+    }
+    float getMaximumInterest()
+    {
+        return maximumInterest;
+    }
+};
+
+class InitialDepositMDS : public MonthlyDepositScheme
+{
+    float maximumInterest;
+    float initialDepositAmount;
+public:
+    InitialDepositMDS(const string& name, float initialBalance, float rate, float depositAmount,float initialAmount const string& accountPrefix)
+        : Account(name, initialBalance, accountPrefix), interestRate(rate), monthlyDepositAmount(depositAmount) {}
+    void setMaximumInterest(float n)
+    {
+        maximumInterest = n;
+    }
+    float getMaximumInterest()
+    {
+        return maximumInterest;
+    }
+    void setInitialDepositAmount(float n)
+    {
+        initialDepositAmount = n;
+    }
+    float getInitialDepositAmount()
+    {
+        return initialDepositAmount;
+    }
+};
+
+int main()
+{
+
+
+}
